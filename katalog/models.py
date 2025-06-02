eb12c8c604c7dd51828f507967962f538403a391
Vojtechpawlik <vojta.pawlik@seznam.cz>
2025-06-02 23:07:35 +0200

    pokracovani

--- katalog/models.py
+++ katalog/models.py
@@ -2,19 +2,26 @@ from django.db import models
 
 class Zanr(models.Model):
     nazev = models.CharField(max_length=100)
+    popis = models.TextField(blank=True)
+    popularita = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
         return self.nazev
 
+
 class Interpret(models.Model):
     jmeno = models.CharField(max_length=100)
+    popis = models.TextField(blank=True)
+    rok_vzniku = models.PositiveIntegerField(null=True, blank=True)
+    zeme_puvodu = models.CharField(max_length=50, blank=True)
 
     def __str__(self):
         return self.jmeno
 
+
 class Album(models.Model):
     nazev = models.CharField(max_length=200)
-    interpret = models.ForeignKey(Interpret, on_delete=models.CASCADE)
+    interpret = models.ForeignKey('Interpret', on_delete=models.CASCADE)
     zanr = models.ForeignKey(Zanr, on_delete=models.SET_NULL, null=True)
     rok = models.PositiveIntegerField()
 
